f7b1ba37fb3ad21ed2919e7f579809562eb0ab1d
Stephen Gary <sgary0@protonmail.com>
2020-09-19 16:19:15 -0400

    added some more dependencies

--- vars/vars.js
+++ vars/vars.js
@@ -1,6 +1,9 @@
 module.exports = {
+  PORT: process.env.PORT || 5000,
+  
   CLIENT_ID: process.env.CLIENT_ID || '718b14e72ce145d2b969634a8128e73a',
   CLIENT_SECRET: process.env.CLIENT_SECRET || 'ad2d309521d747489720eba292603981',
 
   BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 4,
+  JWT_SECRET: process.env.JWT_SECRET || ':)'
 }
